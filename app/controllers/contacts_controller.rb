35b5d16c512f8920f2659702b9765375824e1346
Collin Uemura <ccuemura@gmail.com>
2017-08-01 23:50:21 +0000

    Changed ContactsController

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,7 @@ class ContactsController < ApplicationController
     def create
        @contact = Contact.new(contact_params) 
         
-        if @contact.save
+        if @contact.save#params[:][:] to access forms
             name = params[:contact][:name]
             email = params[:contact][:email]
             body = params[:contact][:comments]
